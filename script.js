e7dae88bff4ab9d96e948b7ceb0e4f67f0a3209a
Reedvejee H. Kiamco <ocmaikreedvejee6@gmail.com>
2026-07-13 16:18:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -58,11 +58,19 @@ document.getElementById("calculateBtn").onclick = function () {
 };
 // Background Slideshow
 const images = [
-  "images/background1.jpg",
-  "images/background6.jpg",
-  "images/background3.jpg",
-  "images/background4.jpg",
-  "images/background5.jpg",
+"images/1.jpg",
+"images/2.jpg",
+"images/3.jpg",
+"images/4.jpg",
+"images/5.jpg",
+"images/6.jpg",
+"images/7.jpg",
+"images/8.jpg",
+"images/9.jpg",
+"images/10.jpg",
+"images/11.jpg",
+"images/12.jpg",
+"images/13.jpg",
 ];
 
 const bg1 = document.getElementById("bg1");
